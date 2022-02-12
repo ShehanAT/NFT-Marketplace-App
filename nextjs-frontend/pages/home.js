5aba6da500ee29fa5fd40c903bfe8044a8563cca
Nipun <shehanatuk@gmail.com>
2022-02-12 17:10:45 -0500

    created home & createNFT pages in nextjs-frontend app

---
+++ nextjs-frontend/pages/home.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+    return (
+        <div className="home-container">
+
+
+        </div>
+
+    );
+}
