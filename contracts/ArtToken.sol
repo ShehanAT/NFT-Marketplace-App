5288b7a94e0ecd80184f9d1999e8e69542056921
Nipun <shehanatuk@gmail.com>
2022-02-22 13:38:49 -0500

    added packages to root dir package.json

--- contracts/ArtToken.sol
+++ contracts/ArtToken.sol
@@ -1,3 +1,55 @@
+
+// pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+// contract ArtToken is ERC721Enumerable {
+//     using Counters for Counters.Counter;
+
+//     Counters.Counter private _tokenIds;
+//     address public marketplace;
+
+//     struct Item {
+//         uint256 id;
+//         address creator;
+//         string uri; // metadata url 
+//     }
+
+//     mapping(uint256 => Item) public Items; // id => Item 
+
+//     constructor() ERC721("ArtToken", "ARTK") {}
+
+//     function mint(string memory uri) public returns (uint256){
+//         _tokenIds.increment();
+//         uint256 newItemId = _tokenIds.current();
+//         _safeMint(msg.sender, newItemId);
+//         approve(marketplace, newItemId);
+
+//         Items[newItemId] = Item({
+//             id: newItemId,
+//             creator: msg.sender,
+//             uri: uri 
+//         });
+
+//         return newItemId;
+//     }
+
+//     function tokenURI(uint256 tokenId) public view override returns (string memory){
+//         require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
+//         return Items[tokenId].uri;
+//     }
+
+//     function setMarketplace(address market) public {
+//         marketplace = market;
+//     }
+
+ 
+// }
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -5,48 +57,45 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+contract ArtToken is ERC721Enumerable{
+  using Counters for Counters.Counter;
 
-contract ArtToken is ERC721Enumerable {
-    using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+  address public marketplace;
 
-    Counters.Counter private _tokenIds;
-    address public marketplace;
+  struct Item {
+    uint256 id;
+    address creator;
+    string uri;//metadata url
+  }
 
-    struct Item {
-        uint256 id;
-        address creator;
-        string uri; // metadata url 
-    }
+  mapping(uint256 => Item) public Items; //id => Item
 
-    mapping(uint256 => Item) public Items; // id => Item 
+  constructor () ERC721("ArtToken", "ARTK") {}
 
-    constructor() ERC721("ArtToken", "ARTK") {}
+  function mint(string memory uri) public returns (uint256){
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
+    _safeMint(msg.sender, newItemId);
+    approve(marketplace, newItemId);
 
-    function mint(string memory uri) public returns (uint256){
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _safeMint(msg.sender, newItemId);
-        approve(marketplace, newItemId);
+    Items[newItemId] = Item({
+      id: newItemId, 
+      creator: msg.sender,
+      uri: uri
+    });
 
-        Items[newItemId] = Item({
-            id: newItemId,
-            creator: msg.sender,
-            uri: uri 
-        });
+    return newItemId;
+  }
 
-        return newItemId;
-    }
+  function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
+    return Items[tokenId].uri;
+  }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory){
-        require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
-        return Items[tokenId].uri;
-    }
+  function setMarketplace(address market) public {
+    //require(msg.sender ==);
+    marketplace = market;
+  }
 
-    function setMarketplace(address market) public {
-        marketplace = market;
-    }
-
-    function safeTransferForm(address payable sellerId, address message, uint256 tokenId) public{
-
-    }
 }
