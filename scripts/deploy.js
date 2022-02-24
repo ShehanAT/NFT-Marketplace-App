fe866c798dfe612de911f64e0883b81828e57aeb
Nipun <shehanatuk@gmail.com>
2022-02-24 13:18:46 -0500

    added scripts/deploy.js file

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main(){
+    const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+    const nftMarket = await NFTMarket.deploy();
+    await nftMarket.deployed();
+    console.log("nftMarket deployed to: " + nftMarket.address);
+
+    const NFT = await hre.ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy(nftMarket.address);
+    await nft.deployed();
+    console.log("nft deployed to: ", nft.address);
+
+    let config = `
+    export config nftmarketaddress = "${nftMarket.address}"
+    export config nftaddress = "${nft.address}"
+    `
+
+    let data = JSON.stringify(config);
+    fs.writeFileSync('config.js', JSON.parse(data));
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
+    
