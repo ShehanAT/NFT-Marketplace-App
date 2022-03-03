a3915bfd24b65408d5028718f795c99cb5ce2da6
Nipun <shehanatuk@gmail.com>
2022-03-03 08:21:17 -0500

    added Select input field to Homepage in nextjs-frontend app

--- nextjs-frontend/src/pages/marketplace/marketplace.js
+++ nextjs-frontend/src/pages/marketplace/marketplace.js
@@ -8,14 +8,24 @@ import Image from 'next/image';
 let rpcEndpoint = "https://rpc-mumbai.maticvigil.com";
 import Web3Modal from "web3modal"
 
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+// import NFTCategory from "../../utils/NFT-Category";
+
 import {
     nftaddress, nftmarketaddress
 } from '../../../../nftConfig';
 
 const Marketplace = () => {
     const [nfts, setNfts] = useState([])
+    const [selectedNftCategory, setSelectedNftCategory] = useState("All")
     const [loadingState, setLoadingState] = useState('not-loaded')
-    
+    const NFTCategories = ["Animals", "Clothes", "Electronics", "Music", "Art"];
+
+
     useEffect(() => {
         loadNFTs()
     }, []);
@@ -24,7 +34,6 @@ const Marketplace = () => {
         const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint)
         const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
         const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
-        
         const data = await marketContract.fetchMarketItems() 
 
         const items = await Promise.all(data.map(async i => {
@@ -62,6 +71,10 @@ const Marketplace = () => {
         loadNFTs()
     }
 
+    // handleCategoryChange = () => {
+
+    // }
+
     if(loadingState === 'Done' && !nfts.length) return (<h1 className="px-20 py10 text-3xl">No items in marketplace</h1>)
     return (
         <>
@@ -71,10 +84,34 @@ const Marketplace = () => {
             <div className="flex justify-center">
                 
                 <div className="px-4" style={{ maxWidth: '1600px' }}>
+                    <div className="grid grid-cols-1">
+                        <Box sx={{ minWidth: 120 }}>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={selectedNftCategory}
+                                label="Select NFT Category"
+                                onChange={(e) => {setSelectedNftCategory(e.target.value);}}
+                                >
+                                    <MenuItem value="All" defaultValue={true} >All</MenuItem>
+                                    {
+                                        NFTCategories.map((key) => { 
+                                            return <MenuItem value={key}>{key}</MenuItem>
+                                        })
+                                    }
+                                </Select>
+                            </FormControl>
+                        </Box>
+                    </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
                         {
+                            (selectedNftCategory == "All") 
+                            ?  
                             nfts.map((nft, i) => {
                                 if(nft.category){
+                                    //  && 
                                     return <div key={i} className="border shadow rounded-xl overflow-hidden">
                                         <Image src={nft.image} width="300px" height="300px"/>
                                         <div className="p-4">
@@ -108,6 +145,28 @@ const Marketplace = () => {
                                     </div>
                                     </div>
                                 }
+                            })
+                            : 
+                            nfts.map((nft, i) => {
+                                console.log(selectedNftCategory);
+                                (nft.category && nft.category == selectedNftCategory) 
+                                ? 
+                                <div key={i} className="border shadow rounded-xl overflow-hidden">
+                                    <Image src={nft.image} width="300px" height="300px"/>
+                                    <div className="p-4">
+                                        <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
+                                        <div style={{ height: '70px', overflow: 'hidden' }}>
+                                            <p className="text-gray-400">{nft.description}</p>
+                                            <p className="text-gray-400">Category {nft.category}</p>
+                                        </div>
+                                    </div>
+                                    <div className="p-4 bg-black">
+                                        <p className="text-2xl mb-4 font-bold text-white">{nft.price} ETH</p>
+                                        <button className="w-full bg-blue-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                                    </div>
+                                </div>
+                                :
+                                <div></div>  
                             })
                         }  
                     </div>
