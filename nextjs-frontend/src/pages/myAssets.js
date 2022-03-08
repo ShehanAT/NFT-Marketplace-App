03be3b55abc71f744d9fe4640a3f5ebfb6d574ed
Nipun <shehanatuk@gmail.com>
2022-03-08 10:31:40 -0500

    added code to files in pages folder

--- nextjs-frontend/src/pages/myAssets.js
+++ nextjs-frontend/src/pages/myAssets.js
@@ -12,5 +12,68 @@ import Market from '../../../artifacts/contracts/Market.sol/NFTMarket.json';
 import NFT from '../../../artifacts/contracts/NFT.sol/NFT.json';
 
 export default function MyAssets(){
-    
+  const [nfts, setNfts] = useState([])
+  const [loadingState, setLoadingState] = useState('loading')
+
+  useEffect(() => {
+      loadNFTs()
+  }, [])
+
+  async function loadNFTs(){
+    const web3Modal = new Web3Modal({
+      network: "mainnet",
+      cacheProvider: true,
+    });
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+    const data = await marketContract.fetchMyNFTs()
+
+    const items = await Promise.all(data.map(async i => {
+        const tokenUri = await marketContract.tokenURI(i.tokenId)
+
+        const meta = await axios.get(tokenUri)
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+        let item = {
+            price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller, 
+            owner: i.owner,
+            image: meta.data.image,
+        }
+        return item 
+    }))
+    setNfts(items)
+    setLoadingState('Done')
+  }
+
+  if(loadingState === "Done" && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets owned</h1>)
+  return (
+      <>
+          <div className="flex justify-center">
+              <h3>MY ASSETS</h3>
+          </div>
+          <div className={styles.container}>
+              <div className="flex justify-center">
+              <div className="p-4">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+                      {
+                          nfts.map((nft, i) => (
+                              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                                  <Image src={nft.image} className="rounded" width="300px" height="300px" />
+                                  <div className="p-4 bg-white">
+                                      <p className="text-2xl font-bold-text-black">Price - {nft.price} Eth</p>
+                                  </div>
+                              </div>
+                          ))
+                      }    
+                  </div> 
+              </div>
+              </div>
+          </div>
+      </>
+  )
+
 }
